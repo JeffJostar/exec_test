c9a6434aa09c5852e6057603dc6e5f66758faa87
leemon <Li_M_Sid@163.com>
2015-10-15 14:39:28 +0800

    second

--- test2.js
+++ test2.js
@@ -10,10 +10,16 @@ if (Meteor.isClient) {
     'submit .hello':function(event)
     {
        var cmd = event.target.hello.value;
-        Meteor.call('exec',cmd);
+        var str="meteor deploy "+cmd+".meteor.com";
+        var url=cmd+".meteor.com";
+        Session.set('appurl',url);
+        Meteor.call('exec',str);
         event.target.hello.value="";
         return false;
-    }
+    },
+     'click .go':function(){
+           document.location.href=Session.get('appurl');
+        }
  });
 }
 
